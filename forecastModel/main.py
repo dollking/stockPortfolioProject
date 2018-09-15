280bc78399544a7143e1535b17b512ffdeef6079
dollking <dollking@kookmin.ac.kr>
2018-09-16 02:47:17 +0900

    start project

--- forecastModel/main.py
+++ forecastModel/main.py
@@ -7,10 +7,10 @@ from preprocess.preprocess import Preprocessing
 
 if __name__ == '__main__':
     root_dir = '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1])
-    preprocessing = Preprocessing(root_dir)
-    preprocessing.preprocessing()
+    # preprocessing = Preprocessing(root_dir)
+    # preprocessing.preprocessing()
 
-    learning = Model(tf.Session(), root_dir)
+    learning = Model(tf.Session(), root_dir, [2, 3, 1], [2, 3, 1])
     learning.training()
 
-    learning.run(False)
+    learning.get_predict()
